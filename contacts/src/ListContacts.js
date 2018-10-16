b275c24c09254e5b514bfc7bd5c65c84d3835dab
suchov <artem.suchov@gmail.com>
2018-10-16 18:43:48 +0300

    initial commit but listing the user name in the component using props

---
+++ contacts/src/ListContacts.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class ListContacts extends Component {
+  render(){
+    return (
+      <ol className='contact-list'>
+        {this.props.contacts.map((person) => (
+        <li key={person.id}>{person.name}</li>
+      ))}
+      </ol>
+    )
+  }
+}
+
+export default ListContacts;
