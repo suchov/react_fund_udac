f9139955b87f8c4a4269b8330b8c45a7c9a65d0c
suchov <artem.suchov@gmail.com>
2018-10-16 23:32:20 +0300

    add PropTypes with yarn - so simple

--- contacts/src/ListContacts.js
+++ contacts/src/ListContacts.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const ListContacts = (props) => {
   return (
@@ -23,6 +24,11 @@ const ListContacts = (props) => {
       ))}
     </ol>
   )
+}
+
+ListContacts.propTypes = {
+  contacts: PropTypes.array.isRequired,
+  onDeleteContact: PropTypes.func.isRequired
 }
 
 export default ListContacts;
